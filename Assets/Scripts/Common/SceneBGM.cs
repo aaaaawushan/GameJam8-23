7d748561b7d644b059a61def0c393c5917110a6b
Celia-veey <2533641203@qq.com>
2026-08-29 12:18:48 +0800

    Implement global volume control, per-scene BGM, and hit SFX

---
+++ Assets/Scripts/Common/SceneBGM.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SceneBGM : MonoBehaviour
+{
+    public AudioSource sceneAudioSource;
+
+    void Start()
+    {
+        if (AudioManager.Instance != null && sceneAudioSource != null && sceneAudioSource.clip != null)
+        {
+            AudioManager.Instance.PlayBGM(sceneAudioSource);
+        }
+    }
+}
